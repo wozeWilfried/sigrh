6dfab0072c3a2fd0cbedcbd63064bfec03275dc1
Woze Wilfried <wilfriedwoze@gmail.com>
2026-05-25 03:27:03 +0200

    test: initialize backend test directory

---
+++ sigrh-backend/src/test/java/com/sigrh/cwa/SigrhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigrh.cwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
